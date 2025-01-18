76fd71c51896615828f4c7936cc660ec951a71b6
Nick Muravev <murvievnick@gmail.com>
2025-01-18 15:05:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.querySelector(".js-our-stories-slider")) {
+    const ourStoriesSlider = new Swiper(".js-our-stories-slider", {
+      slidesPerView: 1.03,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".js-our-stories-slider-next",
+        prevEl: ".js-our-stories-slider-prev",
+      },
+      breakpoints: {
+        560: {
+          spaceBetween: 10,
+          slidesPerView: "auto",
+        },
+        767: {
+          spaceBetween: 20,
+          slidesPerView: "auto",
+        },
+
+        1024: {
+          spaceBetween: 30,
+          slidesPerView: "auto",
+        }
+      },
+    });
+  }
+});
