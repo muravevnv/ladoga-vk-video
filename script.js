59f8f430ad01ea05102b7166bb7002b88527d712
Nick Muravev <murvievnick@gmail.com>
2025-01-21 15:44:46 +0300

    feat: поставил iframe

--- script.js
+++ script.js
@@ -26,20 +26,20 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-const storiesItems = document.querySelectorAll('.our-stories__item');
+// const storiesItems = document.querySelectorAll('.our-stories__item');
 
-if(storiesItems.length > 0) {
-  storiesItems.forEach(item => {
-    const itemLink = item.querySelector('.our-stories__item-preview');
-    const itemFrame = item.querySelector('.our-stories__item-frame');
+// if(storiesItems.length > 0) {
+//   storiesItems.forEach(item => {
+//     const itemLink = item.querySelector('.our-stories__item-preview');
+//     const itemFrame = item.querySelector('.our-stories__item-frame');
     
-    itemLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      const iframeSrc = this.href;
-      const iframe = document.createElement('iframe');
-      iframe.src = iframeSrc;
-      item.classList.add('is-active')
-      itemFrame.appendChild(iframe);
-    });
-  })
-}
+//     itemLink.addEventListener('click', function(event) {
+//       event.preventDefault();
+//       const iframeSrc = this.href;
+//       const iframe = document.createElement('iframe');
+//       iframe.src = iframeSrc;
+//       item.classList.add('is-active')
+//       itemFrame.appendChild(iframe);
+//     });
+//   })
+// }
